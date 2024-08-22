1197e16d421f22a56ab3054e4c66bc2c8e3afa77
Eyal Amir <eyalamirmusic@gmail.com>
2024-08-22 12:31:35 -0700

    Updated test program

--- Plugins/MinimalPluginTemplate/Source/PluginProcessor.cpp
+++ Plugins/MinimalPluginTemplate/Source/PluginProcessor.cpp
@@ -1,17 +1,36 @@
 #include "PluginProcessor.h"
 
+enum class Versions
+{
+    Original,
+    OutOfOrder,
+    InOrder
+};
+
+static Versions getVersion()
+{
+    return Versions::PLUGIN_TEST_VERSION;
+}
+
+inline Param* createParam(const juce::String& name, int version)
+{
+    return new Param({name, version}, name, 0.f, 5.f, 3.f);
+}
+
 MinimalAudioPlugin::MinimalAudioPlugin()
 {
-#if PLUGIN_TEST_VERSION == 2
-    addParameter(c);
-#endif
+    auto version = getVersion();
 
-    addParameter(a);
-    addParameter(b);
+    if (version == Versions::OutOfOrder)
+        addParameter(createParam("c", 2));
 
-#if PLUGIN_TEST_VERSION == 3
-    addParameter(c);
-#endif
+    if (version == Versions::Original || version == Versions::InOrder)
+        addParameter(createParam("a", 1));
+
+    addParameter(createParam("b", 1));
+
+    if (version == Versions::InOrder)
+        addParameter(createParam("c", 2));
 }
 
 void MinimalAudioPlugin::processBlock(juce::AudioBuffer<float>& buffer,
--- Plugins/MinimalPluginTemplate/Source/PluginProcessor.h
+++ Plugins/MinimalPluginTemplate/Source/PluginProcessor.h
@@ -4,11 +4,6 @@
 
 using Param = juce::AudioParameterFloat;
 
-inline Param* createParam(const juce::String& name, int version)
-{
-    return new Param({name, version}, name, 0.f, 5.f, 3.f);
-}
-
 class MinimalAudioPlugin : public PluginHelpers::ProcessorBase
 {
 public:
@@ -16,10 +11,4 @@ public:
     void processBlock(juce::AudioBuffer<float>& buffer,
                       juce::MidiBuffer& midiMessages) override;
 
-    //V1:
-    Param* a = createParam("a", 1);
-    Param* b = createParam("b", 1);
-
-    //V2 or V3:
-    Param* c = createParam("c", 2);
 };
